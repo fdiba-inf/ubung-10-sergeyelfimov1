4a78c37d29f2b5a19e21b7dc0cc832a2f9489ea5
sergeyelfimov1 <sergey.elfimov@fdiba.tu-sofia.bg>
2020-12-01 18:03:37 +0000

    ++

--- src/exercise10/Ellipse.java
+++ src/exercise10/Ellipse.java
@@ -1,48 +1,59 @@
 package exercise10;
 
-public class Ellipse extends Figure {
+public class Ellipse  extends Figure {
     
+
     public Ellipse() {
-        super (new Point (0, 0), 1, 1);
+      super(new Point(0, 0),1,1);
     }
 
     public Ellipse(Point startPoint, double a, double b) {
-       super (startPoint, a, b);
+      super(startPoint, a, b);
+        
     }
 
     public Ellipse(Ellipse otherEllipse) {
-      super (otherEllipse.startPoint, otherEllipse.side1, otherEllipse.side2);
+      super(otherEllipse.startPoint,otherEllipse.side1,otherEllipse.side2);
+        
     }
+
     @Override
-    public double calculatePerimeter(){
-      return Math.PI * (3.0 *(side1 + side2) - Math.sqrt((3.0 * side1 * side2) * (side1 + 3.0 * side2)));
+    public double calculatePerimeter() {
+        return Math.PI * (3.0 * (side1 + side2) - Math.sqrt((3.0 * side1 + side2) * (side1 + 3.0 * side2)));
     }
+
     @Override
     public double calculateArea() {
         return Math.PI * side1 * side2;
     }
+
     @Override
     public String getType() {
         return (side1 == side2) ? "Circle" : "Ellipse";
     }
+
     @Override
     public boolean equal(Figure otherFigure) {
-        if(otherFigure instanceof Ellipse){
-          return super.equal(otherFigure);
-        }else{
-          return false;
-        }
+       if(otherFigure instanceof Ellipse){
+         return super.equal(otherFigure);
+       }
+       else{
+         return false;
+       }
+        
     }
-
+    @Override
     public boolean containsClick(Point click) {
+        // Check if click point is inside the rectangle
         double clickX = click.getX();
         double clickY = click.getY();
 
-        double centerX = startPoint.getX();
-        double centerY = startPoint.getY();
+        double centerX = startPoint.getX()+side1;
+        double centerY = startPoint.getY()+side2;
 
-        double part1 = Math.pow(clickX - centerX, 2) / side1 * side1;
-        double part2 = Math.pow(clickY - centerY, 2) / side2 * side2;
+         double part1 =(Math.pow(clickX - centerX, 2)) / (side1 * side1);
+         double part2 =(Math.pow(clickY - centerY, 2)) / (side2 * side2);
+
         return part1 + part2 <=1;
     }
 }
--- src/exercise10/Rectangle.java
+++ src/exercise10/Rectangle.java
@@ -45,8 +45,9 @@ public class Rectangle extends Figure {
         double startX = startPoint.getX();
         double startY = startPoint.getY();
 
-        boolean xInside = startX < clickX && clickX < (startX + side2);
-        boolean yInside = startY < clickY && clickY < (startY + side1);
-        return false;
+        boolean xInside = startX < clickX && clickX < startX + side2;
+        boolean yInside = startY < clickY && clickY < startY + side1;
+
+        return xInside && yInside;
     }
 }
